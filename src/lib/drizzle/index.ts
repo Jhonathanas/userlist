372e8c65a638561b94b9ecc1b58faa8ef688d5c1
Jhonathanas <jonathanadisaputro99@students.amikom.ac.id>
2025-05-29 23:27:02 +0700

    final

--- src/lib/drizzle/index.ts
+++ src/lib/drizzle/index.ts
@@ -1,8 +1,13 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import { Pool } from 'pg';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
 
-export const db = drizzle(pool);
+// export const db = drizzle(pool);
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
